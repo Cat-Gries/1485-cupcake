4a46d5cd468cc573273e599515a8531996d3b77e
Cat-Gries <cgries7@hotmail.com>
2017-09-05 14:54:57 -0500

    HTML Changes and JS
    Made the changes that were under issues on GitHub. Added the JS also to create a smooth scroll.

---
+++ js/functions.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+jQuery(document).ready(function($){
+	
+		$('.smooth-scroll').on('click', 'a', function(e) {
+   			e.preventDefault();
+		
+		   var hash = this.hash;
+			
+			$('html, body').animate({
+			   scrollTop: $(this.hash).offset().top-38
+			 }, 1000, function(){
+		
+
+			   window.location.hash = hash;
+			 });
+		
+		});
+
+	
+});
